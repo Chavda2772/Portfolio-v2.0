88d8b2f8c596e10a419f4a1984fd4e360be0e783
Chavda Mahesh <chavdamahesh2772@gmail.com>
2024-03-06 01:24:44 +0530

    menu improved

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'selector',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#224295',
+        secondary: '#1F234A',
+        neutral: '#FF4B57',
+      },
+    },
   },
   plugins: [],
 };
